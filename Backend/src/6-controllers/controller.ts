8e54684e69642ba481cf4f75e82276212888ba3d
Michal Resnick <michalf3160@gmail.com>
2023-05-17 02:00:58 +0300

    new gift shop

---
+++ Backend/src/6-controllers/controller.ts
@@ -0,0 +1,46 @@
+import express, { Request, Response, NextFunction } from "express";
+import logic from "../5-logic/logic";
+import GiftModel from "../4-models/gift-model";
+
+const router = express.Router(); // Capital R
+
+// GET http://localhost:3001/api/_____
+router.get("/target-audience", async (request: Request, response: Response, next: NextFunction) => {
+    try {
+       const targetaudience=await logic.getAllTargetAudience()
+       response.json(targetaudience)
+
+
+    }
+    catch (err: any) {
+        next(err);
+    }
+});
+
+router.get("/gifts-per-target-audience/:targetAudienceId", async (request: Request, response: Response, next: NextFunction) => {
+    try {
+       const targetaudienceId=+request.params.targetAudienceId
+       const gifts=await logic.getGiftsByTargetAudience(targetaudienceId)
+       response.json(gifts)
+
+
+    }
+    catch (err: any) {
+        next(err);
+    }
+});
+
+router.post("/gifts", async (request: Request, response: Response, next: NextFunction) => {
+    try {
+       const gift=new GiftModel(request.body)
+       const addedGift=await logic.addGift(gift)
+       response.status(201).json(addedGift)
+
+
+    }
+    catch (err: any) {
+        next(err);
+    }
+});
+
+export default router;
